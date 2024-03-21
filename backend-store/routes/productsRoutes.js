3728d8a81fc5e718226c893e33925970e51be8ee
Omribak <97708552+Omribak@users.noreply.github.com>
2024-03-21 21:01:18 +0200

    Delete backend-store directory

--- backend-store/routes/productsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ProductsController = require('../controllers/ProductsController');
-
-router.route('/').get(ProductsController.getProducts);
-router.route('/:productID').get(ProductsController.getSingleProduct);
-router.route('/search/:searchText').get(ProductsController.getSearchProducts);
-
-module.exports = router;
--- backend-store/routes/stripeRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const StripeController = require('../controllers/StripeController');
-
-router.route('/').post(StripeController.createCheckoutSession);
-
-module.exports = router;
